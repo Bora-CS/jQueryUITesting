bf67a7ea279d411da00a111d27e9deb414388a67
Lixia Sun <110501221+LixiaSun@users.noreply.github.com>
2022-08-29 19:24:41 -0400

    Update DataProviderTest.java

--- src/test/java/dataDriven/DataProviderTest.java
+++ src/test/java/dataDriven/DataProviderTest.java
@@ -1,31 +1,19 @@
 package dataDriven;
 
-import org.testng.Assert;
-
 public class DataProviderTest {
 	
 	
 	
-//  a  b  sum	
-//	3  5  8
-//	4  5  10
-//	3  31 34
-
-
 	public void calculateSumTesting() {
-
-		int[] test1 = {3,5,8};
-		int[] test2 = {4,5,10};
-		int[] test3 = {3,31,34};
+		int a;
+		int b;
+		
+		if 
 		
 		
 		
 		
 		
-		
-		// a+b == sum
-//		Assert.assertEquals((a+b), sum);
-	
 	}
 	
 	
